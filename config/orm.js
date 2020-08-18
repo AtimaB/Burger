2a8fe3728a40de07f49f474de6997ff5f303ab07
AtimaB <nickzehn10@hotmail.com>
2020-08-18 11:05:35 -0600

    first commit

---
+++ config/orm.js
@@ -0,0 +1,70 @@
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(num) {
+    var arr =[];
+    for(var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+
+}
+
+function objToSql(ob) {
+    let arr = [];
+    for (var key in ob) {
+        arr.push(key +"="+ value);
+    }
+    return arr.toString();
+}
+
+
+var orm = {
+    all: function(tableInput, callback){
+        let queryString = "SELECT * FROM " + tableInput +";";
+        connection.query(queryString, function(err, result){
+            if(err){
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    create: function(table, cols, vals , callback) {
+        let queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.lenngth);
+        queryString += ") ";
+
+        console.log(queryString);
+        
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+    update: function(table, objColVals, condition, callback){
+        let queryString = "UPDATE" + table;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+
+        connection.query(queryString, function (err, result){
+            if (err) {
+                throw err;
+            }
+            callback(result);
+        });
+    },
+}
+
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm.js");
+
+let burger = {
+    all: function(callback){
+        orm.all("burgers", function(res){
+            callback(res);
+        });
+    },
+
+    create: function(cols, vals, callback){
+        orm.create("burgers", cols, vals, function(res){
+            callback(res);
+        });
+    },
+
+    update: function(objColVals, condition, callback){
+        orm.update("burgers", objColVals, condition, function(res){
+            callback(res);
+        });
+    },
+};
+
+module.exports = burger;
